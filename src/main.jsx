a5eac95d2f573e54c0edf5050119ec228a9aeebf
xynox19 <saanvi.sethi19@gmail.com>
2025-08-17 13:11:45 +0100

    m..

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '../App';
-import './style.css';
+import './public/style.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
